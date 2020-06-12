7fa153a0538a9bc36f2d579f9c462747eef2bcf1
Oliver Jose Fermin Valdez <oliver_fermin@ucne.edu.do>
2020-06-12 10:18:36 -0400

    Creando modelos

---
+++ Tutorial_EFCore/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial_EFCore.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
---
+++ Tutorial_EFCore/Models/Productos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Tutorial_EFCore.Models
+{
+    public class Productos
+    {
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+
+
+    }
+}
