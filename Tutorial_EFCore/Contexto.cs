c290d1a31a1bf0b72983ef5b871886a389bd4142
Oliver Jose Fermin Valdez <oliver_fermin@ucne.edu.do>
2020-06-12 10:23:26 -0400

    Creando contexto (connection string)

---
+++ Tutorial_EFCore/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using Tutorial_EFCore.Models;
+
+namespace Tutorial_EFCore
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer(@"Server= .\SqlExpress; Database=VentaDB; Trusted_Connection=true;");
+        }
+    }
+}
